8a757f63bce66cfc9d5e4073d5387a186289c2bf
MayankSanodiy <124913875+MayankSanodiy@users.noreply.github.com>
2024-04-25 14:01:26 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,27 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    let left = 0;
+    let right = 0;
+    let charPositions = {};
+    let maxLength = 0;
+
+    while (right < s.length) {
+        let currentChar = s[right];
+
+        if (charPositions.hasOwnProperty(currentChar) && charPositions[currentChar] >= left) {
+            left = charPositions[currentChar] + 1;
+        }
+
+        
+        charPositions[currentChar] = right;
+        maxLength = Math.max(maxLength, right - left + 1);
+        right++;
+    }
+
+    return maxLength;
+     
+    
 }
 
 module.exports = { longestSubstring };
-
 
